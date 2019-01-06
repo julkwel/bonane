d2dcee1f4500a3b9fad9baa2b21e6d4124d0ddb0
julkwel <julienrajerison5@gmail.com>
2019-01-06 07:16:11 +0300

    add github link

--- js/bonane.js
+++ js/bonane.js
@@ -17,17 +17,23 @@ $(document).ready(function () {
 });
 
 /**
+ * Github base_url
+ * @type {string}
+ */
+const baseUrl = 'https://github.com/';
+
+/**
  * Get date
  * @type {number}
  */
-let gasyYear = new Date().getFullYear()
+let gasyYear = new Date().getFullYear();
 
 /**
  * Query html selector add date
  * @type {number}
  */
-document.querySelector('#year').innerText = gasyYear
-document.title += ` ${gasyYear}`
+document.querySelector('#year').innerText = gasyYear;
+document.title += ` ${gasyYear}`;
 
 /**
  * Prevent message on null, undefined, '', 0, NaN, false
@@ -78,28 +84,47 @@ function checkName(nous) {
 }
 
 /**
+ * Check github link
+ * @param link
+ * @returns {*}
+ */
+function checkGithub(link) {
+    if (link) {
+        return baseUrl+link;
+    } else {
+        return 'https://github.com/';
+    }
+}
+
+/**
  * Fetch json file
  */
 $.getJSON('USER.json', function (elements) {
     elements.forEach(function (nous, i) {
         document.getElementById('carousel-indicators').innerHTML += `
-              <li data-target="#quote-carousel" data-slide-to="${i}" class="${isActive(i)}" title="${nous.name}">
-                <img src="${nous.image}" alt="">
-              </li>
-    `
+              <li data-target="#quote-carousel" data-slide-to="${i}" class="${isActive(i)}" title="${checkName(nous.name)}">
+                <img src="${checkImage(nous.image)}" alt="">
+              </li>`
+
         document.getElementById('name').innerHTML += `
-            <div class="item ${isActive(i)}">\n
-               <blockquote>\n
-                  <div class="row">\n
-                       <div class="col-sm-3 text-center">\n
-                           <img class="img-circle" src="${checkImage(nous.image)}" style="width: 100px;height:100px;">\n
-                       </div>\n
-                        <div class="col-sm-9">\n
-                            <p>${formatMessage(nous.message)}</p>\n
-                            <small>  ${checkName(nous.name)}  </small>\n
-                        </div>\n
-                    </div>\n
-               </blockquote>\n
+            <div class="item ${isActive(i)}">
+               <blockquote>
+                  <div class="row">
+                       <div class="col-sm-3 text-center">
+                           <img class="img-circle" src="${checkImage(nous.image)}" style="width: 100px;height:100px;">
+                       </div>
+                        <div class="col-sm-9">
+                            <p>${formatMessage(nous.message)}</p>
+                            <div class="row">
+                                <small>
+                                    ${checkName(nous.name)}
+                                    <a href="${checkGithub(nous.username)}" target="_blank" 
+                                    style="color: #f9f9f9;margin: 5px"><i class="fa fa-github"></i></a>
+                                </small>
+                            </div>
+                        </div>
+                    </div>
+               </blockquote>
             </div>`
     })
 })
